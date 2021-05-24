63081e3b3827315f3063fcac6e73d50cf25c4a08
ZELLAT Abdelkhalek <ha_zellat@esi.dz>
2021-05-24 03:36:55 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,70 @@ import {
   Switch,
   Route,
   Redirect,
+  NavLink,
 } from "react-router-dom";
 import GeneticAlgorithm from "./screens/GeneticAlgorithm";
 import SimulatedAnnealing from "./screens/SimulatedAnnealing";
+import {
+  Nav,
+  NavItem,
+  Dropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu,
+} from "reactstrap";
+
 export default class App extends Component {
   render() {
     return (
       <Router>
+        <ul class="nav nav-pills mb-2 p-2">
+          <li class="nav-item">
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              to="/dynamic-programming"
+            >
+              Dynamic Programming
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink activeClassName="active" className="nav-link" to="/greedy">
+              Greedy
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              to="/hill-climbing"
+            >
+              Hill Climbing
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              to="/simulated-annealing"
+            >
+              Simulated annealing
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              to="/genetic-algorithm"
+            >
+              Genetic algorithm
+            </NavLink>
+          </li>
+        </ul>
         <Switch>
+          <Route path="/dynamic-programming">
+            <SimulatedAnnealing />
+          </Route>
           <Route path="/simulated-annealing">
             <SimulatedAnnealing />
           </Route>
