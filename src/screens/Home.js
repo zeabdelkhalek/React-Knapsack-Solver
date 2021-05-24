2973613c1fbfe32b73f39dd31cc8e8717e8188c9
ZELLAT Abdelkhalek <ha_zellat@esi.dz>
2021-05-24 04:27:10 +0100

    Fix css and add image

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,11 +4,17 @@ import { Button, Label, Input, Jumbotron, Badge } from "reactstrap";
 export default class Homne extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
         <Jumbotron>
-          <h1 className="display-9 text-center">
-            Welcome To The Knapsack-Problem Sover
-          </h1>
+          <h1 className="display-4">Welcome To The Knapsack-Problem Solver</h1>
+          <p class="lead">
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <img
+            height="500"
+            src="https://miro.medium.com/max/4298/1*4b1qYRRYPnPvOhT5UIYP1g.png"
+          />
         </Jumbotron>
       </div>
     );
